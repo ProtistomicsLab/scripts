56a56d2b07b1aa4ff5d18a9fd4c3d880b30baf05
Monika Wisniewska <40297172+Jadziaa@users.noreply.github.com>
2025-03-21 13:34:37 +0100

    Add files via upload

---
+++ fasta_to_phylip.py
@@ -0,0 +1,26 @@
+from Bio import AlignIO
+from pathlib import Path
+from Bio.AlignIO.PhylipIO import SequentialPhylipWriter
+import re
+import sys
+
+file = open(sys.argv[1])
+file_name=sys.argv[1]
+out_name = file_name.split(".")[0]
+out_name = out_name + ".phy"
+
+
+print(out_name)
+
+
+#fileoutnex = re.sub('fasta', 'nex', file)
+#pathway = '/mypath/'
+alignment = AlignIO.read(file, 'fasta')
+#print(alignment)
+# write out in nexus
+#AlignIO.write(alignment, Path(pathway, fileoutnex), 'nexus')
+
+# write out in relaxed phylip
+#fileoutphy = re.sub('fasta', 'phy', file)
+with open(out_name, 'w') as output_handle:        
+    SequentialPhylipWriter(output_handle).write_alignment(alignment, id_width=30)
